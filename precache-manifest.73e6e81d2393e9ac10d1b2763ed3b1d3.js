835713565c2557d43fe8fdd2bb03a6c011e8201e
mohamed gamal <m.yassen.93@gmail.com>
2020-07-12 22:35:26 +0200

    Updates

---
+++ precache-manifest.73e6e81d2393e9ac10d1b2763ed3b1d3.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c833d2b208045e02a3de5bca9e1c2d91",
+    "url": "/dashboard/index.html"
+  },
+  {
+    "revision": "2192e71ecb2396ae05b6",
+    "url": "/dashboard/static/css/2.7d014b95.chunk.css"
+  },
+  {
+    "revision": "40d7c1ce3fce979edc7c",
+    "url": "/dashboard/static/css/main.3424eee9.chunk.css"
+  },
+  {
+    "revision": "2192e71ecb2396ae05b6",
+    "url": "/dashboard/static/js/2.3ce6cea5.chunk.js"
+  },
+  {
+    "revision": "d2966845b94a3318bf32eecc7af8015d",
+    "url": "/dashboard/static/js/2.3ce6cea5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "40d7c1ce3fce979edc7c",
+    "url": "/dashboard/static/js/main.d3048b6c.chunk.js"
+  },
+  {
+    "revision": "6247521c80abc44689c3",
+    "url": "/dashboard/static/js/runtime-main.ab998596.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/dashboard/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/dashboard/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/dashboard/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/dashboard/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/dashboard/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
